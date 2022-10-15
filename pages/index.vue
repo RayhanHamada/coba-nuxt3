4ea2807852743f7744504457cedc9d2fde24d123
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-10-15 18:31:39 +0700

    chore: init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+const count = useState('counter', () => 0);
+
+</script>
+
+<template>
+    <div>
+        <div>{{ count }}</div>
+        <button @click="count++">Increment</button>
+    </div>
+</template>
